eb92a84e1bd4ed082e9a50cde9552d8f6a3ab8cc
Vinay Kumar J N <javalkar.vinaykumar@gmail.com>
2023-01-27 11:19:45 +0530

    socket program practice

--- src/main/java/com/learning/practice/net/SocketController.java
+++ src/main/java/com/learning/practice/net/SocketController.java
@@ -1,5 +1,7 @@
 package com.learning.practice.net;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,11 @@ public class SocketController {
 
 	@GetMapping("/socket")
 	public void sendMessageViaSocket(String msg) {
-		service.sendMessage(msg);
+		try {
+			service.sendMessage(msg);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
--- src/main/java/com/learning/practice/net/SocketService.java
+++ src/main/java/com/learning/practice/net/SocketService.java
@@ -1,11 +1,37 @@
 package com.learning.practice.net;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class SocketService {
 
-	public void sendMessage(String msg) {
-		
+	public void sendMessage(String msg) throws IOException {
+		Socket socket = new Socket("localhost", 8001);
+
+        // Send an HL7 message to the server
+        OutputStream os = socket.getOutputStream();
+        OutputStreamWriter osw = new OutputStreamWriter(os);
+        BufferedWriter bw = new BufferedWriter(osw);
+        bw.write(msg);
+        bw.flush();
+
+        // Receive acknowledgement from the server
+        InputStream is = socket.getInputStream();
+        InputStreamReader isr = new InputStreamReader(is);
+        BufferedReader br = new BufferedReader(isr);
+        String ack = br.readLine();
+        System.out.println("Server acknowledged: " + ack);
+
+        // Close the socket
+        socket.close();
 	}
 }
